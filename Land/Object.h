c24f06430f83012e6158b1b5c1c568d7d92771f9
Andrew Huss <dangerhuss@gmail.com>
2012-01-26 17:33:30 -0800

    Initial Commit!!!

---
+++ Land/Object.h
@@ -0,0 +1,13 @@
+//
+//  Object.h
+//  Land
+//
+//  Created by Andrew Huss on 1/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Object : NSObject
+
+@end
---
+++ Land/Object.m
@@ -0,0 +1,13 @@
+//
+//  Object.m
+//  Land
+//
+//  Created by Andrew Huss on 1/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Object.h"
+
+@implementation Object
+
+@end
