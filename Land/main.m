c419abcea4a403bae81bca9b47534e7d0001d7c3
Andrew Huss <dangerhuss@gmail.com>
2012-01-26 16:19:43 -0800

    Initial Commit

---
+++ Land/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Land
+//
+//  Created by Andrew Huss on 1/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MAKJAMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([MAKJAMAppDelegate class]));
+	}
+}
---
+++ LandTests/LandTests.h
@@ -0,0 +1,13 @@
+//
+//  LandTests.h
+//  LandTests
+//
+//  Created by Andrew Huss on 1/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LandTests : SenTestCase
+
+@end
