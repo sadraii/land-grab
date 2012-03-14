b5456a0546c106c7d0f11460a36648f7cf99cf59
student <student@ssu-cs-dar25-167-189.sonoma.edu>
2012-03-14 14:30:46 -0700

    Jason: 	Added clock skeleton 	Need to finish clock with NSTimer and link it to gameplay

--- LandGrab!/MJClockWidget.m
+++ LandGrab!/MJClockWidget.m
@@ -35,16 +35,40 @@
 {
     // Drawing code
  
-    NSInteger offset = 300;
-    
+    // Draw outter circle
     rect = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);     
     CGContextRef contextRef = UIGraphicsGetCurrentContext();            
     CGContextSetLineWidth(contextRef, 0.5);                  
     CGContextSetRGBFillColor(contextRef, 255, 255, 255, 1);             
-    CGContextSetRGBStrokeColor(contextRef, 255, 255, 255, 1);            
+    CGContextSetRGBStrokeColor(contextRef, 0, 0, 0, 1);            
     CGContextFillEllipseInRect(contextRef, rect);                 
     CGContextStrokeEllipseInRect(contextRef, rect);  
     
+    NSInteger offset = 5;
+       
+    // Draw inner circle
+    rect = CGRectMake(0+(offset/2), 0+(offset/2), self.frame.size.width-offset, self.frame.size.height-offset);                 
+    CGContextSetLineWidth(contextRef, 0.5);                  
+    CGContextSetRGBFillColor(contextRef, 0, 0, 0, 0.5f);
+    CGContextSetRGBStrokeColor(contextRef, 0.0, 0.0, 0.0, 0.5f);              
+    
+    CGContextFillEllipseInRect(contextRef, rect);                 
+    CGContextStrokeEllipseInRect(contextRef, rect);     
+    
+    // Draw lines
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextTranslateCTM(context, (self.frame.size.width/2), (self.frame.size.height/2)); 
+    for (int x=0; x<270; x++) {  
+        
+        CGContextSetLineWidth(context, 0.5);
+        CGContextSetRGBStrokeColor(context, 0.0, 0.0, 0.0, 1);
+        //CGContextMoveToPoint(context, (self.frame.size.width/2), (self.frame.size.height/2));   
+        CGContextMoveToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, ((self.frame.size.width/2)*(cos((x*1)*(M_PI/180)))), ((self.frame.size.height/2)*(sin((x*1)*(M_PI/180)))));                   
+        CGContextStrokePath(context);                           
+        CGContextSetFillColorWithColor(context, [UIColor whiteColor].CGColor);       
+        CGContextFillPath(context);
+    }    
 }
 
 
