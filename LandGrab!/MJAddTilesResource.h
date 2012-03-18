0000a5c3eb0ba035e631abacb3b183509977cb72
Maximilian Maksutovic <maksutovic@gmail.com>
2012-03-18 16:55:44 -0700

    - Added MJAddTilesResource Class

---
+++ LandGrab!/MJAddTilesResource.h
@@ -0,0 +1,13 @@
+//
+//  MJAddTilesResource.h
+//  LandGrab!
+//
+//  Created by Helen Saenz on 3/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MJResource.h"
+
+@interface MJAddTilesResource : MJResource
+
+@end
